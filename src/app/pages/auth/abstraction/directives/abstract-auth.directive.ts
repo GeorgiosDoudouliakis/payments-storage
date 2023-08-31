11077cc679578947d454aad5a20eafbe63f7271f
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-08-31 23:53:09 +0300

    refactor: Initialize authForm property on constructor

--- src/app/pages/auth/abstraction/directives/abstract-auth.directive.ts
+++ src/app/pages/auth/abstraction/directives/abstract-auth.directive.ts
@@ -1,17 +1,18 @@
 /* Place your angular imports here */
-import { Directive, OnInit } from '@angular/core';
+import { Directive } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Directive()
-export abstract class AbstractAuthDirective implements OnInit {
-  public authForm!: FormGroup;
+export abstract class AbstractAuthDirective {
+  public authForm: FormGroup;
   public isPasswordVisible: boolean = false;
   public abstract pageTitle: string;
 
-  constructor(private _fb: FormBuilder) {}
-
-  public ngOnInit(): void {
-    this._initializeForm();
+  constructor(private _fb: FormBuilder) {
+    this.authForm = this._fb.group({
+      email: this._fb.control<string>("", [Validators.required, Validators.email]),
+      password: this._fb.control<string>("", [Validators.required, Validators.minLength(6)])
+    });
   }
 
   public abstract submitAction(): void;
@@ -19,12 +20,5 @@ export abstract class AbstractAuthDirective implements OnInit {
   public onSubmit(): void {
     if(this.authForm.invalid) return;
     this.submitAction();
-  }
-
-  private _initializeForm(): void {
-    this.authForm = this._fb.group({
-      email: this._fb.control<string>("", [Validators.required, Validators.email]),
-      password: this._fb.control<string>("", [Validators.required, Validators.minLength(6)])
-    });
   }
 }
