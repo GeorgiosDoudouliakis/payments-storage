1a76631928bf53334594874da8e6a19da4b39b16
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-09-02 14:41:18 +0300

    refactor: Replace dropdown mobile menu with drawer and remove border bottom style from ul

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -4,21 +4,29 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 /* Place your module imports here */
-import { NzMenuModule } from "ng-zorro-antd/menu";
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzDropDownModule } from "ng-zorro-antd/dropdown";
+import { NzDrawerModule } from "ng-zorro-antd/drawer";
 
 @Component({
   selector: 'ps-navigation',
   standalone: true,
-  imports: [CommonModule, RouterLinkActive, RouterLink, NzMenuModule, NzIconModule, NzDropDownModule],
+  imports: [CommonModule, RouterLinkActive, RouterLink, NzIconModule, NzDrawerModule],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
   public isOnMobile: boolean = false;
+  public isDrawerVisible: boolean = false;
 
   public ngOnInit(): void {
     this.isOnMobile = window.matchMedia("(max-width: 767px)").matches;
+  }
+
+  public openDrawer(): void {
+    this.isDrawerVisible = true;
+  }
+
+  public closeDrawer(): void {
+    this.isDrawerVisible = false;
   }
 }
