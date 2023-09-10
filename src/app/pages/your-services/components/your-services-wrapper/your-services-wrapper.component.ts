15e317ecd79bfc5971159fe9b2652489383f9f43
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-09-10 18:49:58 +0300

    refactor: Use top tab position before 1024px and left after

--- src/app/pages/your-services/components/your-services-wrapper/your-services-wrapper.component.ts
+++ src/app/pages/your-services/components/your-services-wrapper/your-services-wrapper.component.ts
@@ -1,4 +1,8 @@
+/* Place your angular imports here */
 import { Component } from '@angular/core';
+
+/* Place any other imports here */
+import { NzTabPosition } from "ng-zorro-antd/tabs";
 
 @Component({
   selector: 'ps-your-services-wrapper',
@@ -6,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./your-services-wrapper.component.scss']
 })
 export class YourServicesWrapperComponent {
+  public tabPosition: NzTabPosition;
 
+  constructor() {
+    this.tabPosition = window.matchMedia('(max-width: 1023px)').matches ? "top" : "left";
+  }
 }
