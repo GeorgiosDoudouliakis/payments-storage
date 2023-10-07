c104297e297d444d88bd6de47e007647e0a89ed3
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-10-07 15:39:44 +0300

    refactor: Remove service deletion from service table item and change the button type of more link

--- src/app/pages/your-services/components/service-table-item/service-table-item.component.ts
+++ src/app/pages/your-services/components/service-table-item/service-table-item.component.ts
@@ -5,7 +5,6 @@ import { NgIf, NgOptimizedImage } from "@angular/common";
 /* Place your module imports here */
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzIconModule } from "ng-zorro-antd/icon";
-import { NzPopconfirmModule } from "ng-zorro-antd/popconfirm";
 
 /* Place your interface imports here */
 import { YourServiceInterface } from "../../interfaces";
@@ -17,12 +16,10 @@ import { RouterLink } from "@angular/router";
 @Component({
   selector: 'ps-service-table-item',
   standalone: true,
-  imports: [NzButtonModule, NzIconModule, NzPopconfirmModule, FirstTwoLettersPipe, NgOptimizedImage, NgIf, RouterLink],
+  imports: [NzButtonModule, NzIconModule, FirstTwoLettersPipe, NgOptimizedImage, NgIf, RouterLink],
   templateUrl: './service-table-item.component.html',
   styleUrls: ['./service-table-item.component.scss']
 })
 export class ServiceTableItemComponent {
   @Input() public yourService: YourServiceInterface;
-
-  public confirmServiceDeletion(): void {}
 }
